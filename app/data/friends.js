c8c7139864a72868e053c24a6867c18be80cecce
Miles Kong <mileskong@Miless-Air.fios-router.home>
2018-03-17 06:53:49 -0400

    completed home.html file

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+
+var friendData = [
+	{
+		name: "Arnold Scharzenegger",
+		photo: '<a href="http://tinypic.com?ref=suyaeu" target="_blank"><img src="http://i65.tinypic.com/suyaeu.jpg" border="0" alt="Image and video hosting by TinyPic"></a>',
+		scores: [3,3,3,3,3,3,3,3,3,3]
+	},
+     {
+		name: "Sylvester Stallone",
+		photo: '<a href="http://tinypic.com?ref=fns4eb" target="_blank"><img src="http://i64.tinypic.com/fns4eb.jpg" border="0" alt="Image and video hosting by TinyPic"></a>',
+		scores: [4,4,4,4,4,4,4,4,4,4]
+	},
+     {
+		name: "Mila Jovivich",
+		photo: '<a href="http://tinypic.com?ref=2w30s1y" target="_blank"><img src="http://i67.tinypic.com/2w30s1y.jpg" border="0" alt="Image and video hosting by TinyPic"></a>',
+		scores: [5,5,5,5,5,5,5,5,5,5]
+	}
+];
+
+module.exports = friendData;
